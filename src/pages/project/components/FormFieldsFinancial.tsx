ceabb6319126a31e4c2e87531b7ffb706fd383e7
ana.anzini <anabeanzini@gmail.com>
2024-11-20 11:23:42 -0300

    Remoção de linha duplicada e forms

--- src/pages/project/components/FormFieldsFinancial.tsx
+++ src/pages/project/components/FormFieldsFinancial.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Select, Col, Row, Checkbox } from 'antd';
+import { Form, Input, Select, Col, Row, Checkbox, InputNumber } from 'antd';
 import { StatusType } from '../IProject';
 import { statusTypeLabels } from './ProjectTable';
 
@@ -10,6 +10,34 @@ interface IFormFieldsFinancialProps {
 const FormFields: React.FC<IFormFieldsFinancialProps> = ({ form }) => {
     return (
         <Row gutter={16}>
+            <Col span={4}>
+                <Form.Item
+                    name="totalValue"
+                    label="Valor Total dos Produtos (R$)"
+                >
+                    <InputNumber
+                        min={0}
+                        style={{ width: "100%" }}
+                        precision={2}
+                        step={0.01}
+                        placeholder="Digite o valor"
+                        disabled={true}
+                    />
+                </Form.Item>
+            </Col>
+            <Col span={2}>
+                <Form.Item
+                    name="installmentNumber"
+                    label="N. Parcelas"
+                    rules={[{ required: true, message: "Campo obrigatório" }]}
+                >
+                    <InputNumber
+                        min={0}
+                        style={{ width: "100%" }}
+                        placeholder="Digite o valor"
+                    />
+                </Form.Item>
+            </Col>
         </Row>
     );
 };
