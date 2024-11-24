eec0d38be7450d315ae4d5e0eaf6d5d079ce217a
ana.anzini <anabeanzini@gmail.com>
2024-11-24 10:27:34 -0300

    Expiração do token

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Notification } from "../components/notification/Notification";
 
 const api = axios.create({
     baseURL: 'http://localhost:8081/',
@@ -16,5 +17,19 @@ api.interceptors.request.use(config => {
 }, error => {
     return Promise.reject(error);
 });
+
+//Sessão expirada
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            sessionStorage.removeItem('token');
+            Notification({ type: "error", message: "Sessão expirada. Por favor, faça login novamente." });
+            window.location.href = '/';
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default api;
