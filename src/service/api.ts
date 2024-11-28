7e10962d6a9deea896dd16492b8651e0e214fb39
ana.anzini <anabeanzini@gmail.com>
2024-11-28 20:32:06 -0300

    Update api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Notification } from "../components/notification/Notification";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081/',
+    baseURL: 'https://mobisoft.site/api',
     headers: {
         "Content-type": "application/json",
     },
