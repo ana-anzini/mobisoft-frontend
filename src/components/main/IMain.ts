869680b9bbb2108ddaa6874b42217650061cfa2e
ana.anzini <anabeanzini@gmail.com>
2024-11-29 00:53:52 -0300

    Retirada do main

--- src/components/main/IMain.ts
+++
@@ -1,8 +0,0 @@
-import { CSSProperties, ReactNode } from "react";
-
-export interface IMain {
-    mainButtonTitle: string;
-    mainButtonIcon?: ReactNode;
-    handleEdit: (a: any) => void
-    handleDelete: (a: any) => void
-}
